312a4afe110c32ddd94e87aac52932f5734935e5
NoseQuePonerUAX <joserafe@myuax.com>
2024-03-10 21:49:30 +0100

    si

--- GestiondeHabitats/src/main/java/org/example/Main.java
+++ GestiondeHabitats/src/main/java/org/example/Main.java
@@ -1,6 +1,17 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    protected String nombre;
+    protected String tipo;
+    protected double temperatura;
+    protected double humedad;
+    protected double limpieza;
+
+    public (String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.temperatura = 0;
+        this.humedad = 0;
+        this.limpieza = 0;
     }
 }
